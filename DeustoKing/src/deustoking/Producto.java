dbc567d4bebc66a80434b0741203328538d904fa
Aitor de la Osa <aitordela.osa@opendeusto.es>
2023-11-06 08:18:17 +0100

    Creacion clase Producto

---
+++ DeustoKing/src/deustoking/Producto.java
@@ -0,0 +1,43 @@
+package deustoking;
+
+public class Producto {
+	private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Cantidad: " + cantidad + ", Precio: $" + precio;
+    }
+
+}
