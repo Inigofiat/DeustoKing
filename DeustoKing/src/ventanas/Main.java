6a0e6530b970cf5d88d09643054fc4776d2c44e2
Aitor de la Osa <aitordela.osa@opendeusto.es>
2023-11-19 18:37:11 +0100

    Añadido al main el logger

--- DeustoKing/src/ventanas/Main.java
+++ DeustoKing/src/ventanas/Main.java
@@ -1,10 +1,30 @@
 package ventanas;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
 
 	public static void main(String[] args) {
 		VentanaCargando vc = new VentanaCargando();
-
+		Logger logger = java.util.logging.Logger.getLogger("Logger");
+		
+		try {
+			FileInputStream fis = new FileInputStream("src/conf/logger.properties");
+			LogManager.getLogManager().readConfiguration(fis);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
