d1911799e4c3111cfff6c7934c2f1ab598eace84
Iñigo Fiat <inigo.fiat@opendeusto.es>
2023-11-19 14:59:56 +0100

    Acción del botón reservas creado

--- DeustoKing/src/ventanas/VentanaPrincipal.java
+++ DeustoKing/src/ventanas/VentanaPrincipal.java
@@ -73,6 +73,16 @@ public class VentanaPrincipal extends JFrame {
 		btnReservas.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 18));
 		btnReservas.setMargin(new Insets(10, 10, 10, 10));
 		panBotones.add(btnReservas);
+		btnReservas.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new VentanaReserva();
+				vActual.setVisible(false);
+				vActual.dispose();
+				
+			}
+		});
 		
 		btnCupon = new JButton("CUPONES");
 		btnCupon.setBackground(new Color(189,206,243));
