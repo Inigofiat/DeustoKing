c60ab337f8a4a95f08b33ead725e9632f23f99d7
Iñigo Fiat <inigo.fiat@opendeusto.es>
2023-11-21 08:52:26 +0100

    Establecer el logger como atributo public

--- DeustoKing/src/ventanas/Main.java
+++ DeustoKing/src/ventanas/Main.java
@@ -8,7 +8,7 @@ import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
-	private static Logger logger = Logger.getLogger(Main.class.getName());
+	static Logger logger = Logger.getLogger(Main.class.getName());
 	public static void main(String[] args) {
 		VentanaCargando vc= new VentanaCargando();
 		try {
