60ea584918b63bc72f559e64307ec48d23867a7a
Naroa Azcona <naroa.azcona@opendeusto.es>
2023-11-29 17:35:30 +0100

    Metodos de registro añadidos

--- DeustoKing/src/deustoking/Restaurante.java
+++ DeustoKing/src/deustoking/Restaurante.java
@@ -19,6 +19,7 @@ import java.util.TreeSet;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 public class Restaurante {
@@ -27,6 +28,7 @@ public class Restaurante {
 	private static Set<Reserva> setReservas;
 	private static List<Reserva> listaReservas;
 	private static Map<String, Cliente> mapaPersonaPorCorreo;
+	private static List<Cliente> clientes;
 	Cliente cliente;
 	
 	public Restaurante() {
@@ -125,6 +127,130 @@ public class Restaurante {
 			}
 		}
 		return lp;
+	}
+	
+
+	public static Cliente buscarUsuario(String inicio) {
+	    boolean enc = false;
+	    int pos = 0;
+	    Cliente c = null;
+
+	    while (!enc && pos < clientes.size()) {
+	        c = clientes.get(pos);
+	        if (( c.getCorreo().equals(inicio)) ||
+	            ( c.getNombreUsuario().equals(inicio)) ||
+	            ( c.getTelefono().equals(inicio))) {
+	            enc = true;
+	        } else {
+	            pos++;
+	        }
+	    }
+
+	    if (enc) {
+	        return c;
+	    } else {
+	        return null;
+	    }
+	}
+
+	public static void guardarClientes(String nomfichClientes) {
+		try {
+			PrintWriter pw = new PrintWriter(nomfichClientes);
+			for(Cliente c: clientes) {
+				pw.println(c.getNombre()+";"+c.getApellidos()+";"+c.getTelefono()+";"+c.getDireccion()+";"+c.getCorreo()+";"+
+						c.getNombreUsuario()+";"+c.getContrasenia());
+			}
+			pw.flush();
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String obtenerParametroBusquedaInicioSesion(String email, String nombre, String tlf) {
+	    
+	    if (!email.isEmpty()) {
+	        return email;
+	    } else if (!nombre.isEmpty()) {
+	        return nombre;
+	    } else {
+	        return tlf;
+	    }
+	}
+
+	public static boolean verificarContrasenia(String contrasenia) {
+
+		if(contrasenia.length() < 6 || !contrasenia.matches(".*[0-9].*") || !contrasenia.matches(".*[a-zA-Z].*") || !contrasenia.matches(".*[!@#$%^&*()-_=+{};:,<.>/?`~].*")) {
+			JOptionPane.showMessageDialog(null, "La contraseña no contiene los caracteres necesarios", "ERROR", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+	
+	
+	public static boolean verificarNombreUsuario(String nombreUsuario) {
+		if(!nombreUsuario.matches(".*[a-zA-Z].*")&& nombreUsuario.length()<4) {
+			JOptionPane.showMessageDialog(null, "El nombre de usuario es incorrecto", "ERROR", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+	
+
+	public static boolean verificarTelefono(String telefono) {
+		if(telefono.length()!=9 || !telefono.matches(".*[0-9].*") || telefono.matches(".*[a-zA-Z].*")) {
+			JOptionPane.showMessageDialog(null, "El numero de teléfono es incorrecto", "ERROR", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+
+	
+	public static void aniadirCliente(Cliente c) {
+		clientes.add(c);
+	}
+	
+
+	public static boolean registroCliente(String nomfichCliente, String nombre, String apellido, String telefono, String direccion, String correo,
+		int id, int puntos, String nombreUsuario, String contrasenia) {
+
+		if(verificarContrasenia(contrasenia)&& verificarNombreUsuario(nombreUsuario)&&verificarTelefono(telefono)) {
+			Cliente nuevoCliente = new Cliente(nombre, apellido, telefono,direccion, correo, 1, 0 , nombreUsuario, contrasenia);
+			clientes.add(nuevoCliente);
+			guardarClientes(nomfichCliente);
+			return true;
+		} 
+		return false;
+	}
+	
+	
+	public static void cargarClientesEnLista(String nombfich) {
+		try {
+			Scanner sc = new Scanner(new File(nombfich));
+			while(sc.hasNextLine()) {
+				String linea = sc.nextLine();
+				String[]partes = linea.split(";");
+				String nombre = partes[0];
+				String apellidos = partes[1];
+				String tlf = partes[2];
+				String email = partes[3];
+				String dir = partes[4];
+				String nUsuario = partes[5];
+				String contrasenia = partes[6];
+				
+				 String parametroBusqueda = obtenerParametroBusquedaInicioSesion(email, nombre, tlf);
+				if(buscarUsuario(parametroBusqueda)==null) {
+					Cliente cliente = new Cliente(nombre, apellidos, tlf, email, dir, Persona.getContador(), 0, nUsuario, contrasenia);
+					clientes.add(cliente);
+					
+				}
+			}
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 	}
 	
 }
