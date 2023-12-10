ce878b90f79da00f0e4c3c7f835592b174f4b1be
Aitor de la Osa <aitordela.osa@opendeusto.es>
2023-12-10 16:28:13 +0100

    logger ventana ensaladas

--- DeustoKing/src/ventanas/VentanaEnsaladas.java
+++ DeustoKing/src/ventanas/VentanaEnsaladas.java
@@ -8,6 +8,8 @@ import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -29,6 +31,7 @@ public class VentanaEnsaladas extends JFrame {
 	private JScrollPane barra; 
 	private JLabel lblTitulo;
 	private JFrame vActual, vAnterior;
+	static Logger logger = Logger.getLogger(Main.class.getName());
 	
 	public VentanaEnsaladas(JFrame va) {
 		super();
@@ -131,7 +134,9 @@ public class VentanaEnsaladas extends JFrame {
 		btnVolver.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 15));
 		btnVolver.setMargin(new Insets(10, 10, 10, 10));
 		btnVolver.setBorderPainted(true);
+		
 		btnVolver.addActionListener((e)->{
+			logger.log(Level.INFO, "SE HA CLICKADO EL BOTON VOLVER");
 			vActual.dispose();
 			vAnterior.setVisible(true);
 		});
