679efa6c5f9f46cf406560911db6830ce81b42a9
Iñigo Fiat <inigo.fiat@opendeusto.es>
2023-12-10 16:53:09 +0100

    Base de datos añadida al main

--- DeustoKing/src/ventanas/Main.java
+++ DeustoKing/src/ventanas/Main.java
@@ -3,12 +3,18 @@ package ventanas;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+import basesDeDatos.BD;
+import deustoking.Producto;
+import deustoking.Restaurante;
+
 public class Main {
-	static Logger logger = Logger.getLogger(Main.class.getName());
+	public static Logger logger = Logger.getLogger(Main.class.getName());
 	public static void main(String[] args) {
 		VentanaCargando vc= new VentanaCargando();
 		try {
@@ -17,5 +23,16 @@ public class Main {
 
 			e.printStackTrace();
 		}
+		
+		Connection con = BD.initBD("deustoking.db");
+		try {
+			BD.crearTabla(con);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		Restaurante.volcarCSVPersonasABD(con, "Clientes.csv");
+		Restaurante.volcarCSVReservasABD(con, "reservas.csv");
+		BD.insertarProducto(con, new Producto(0, "Hamburguesa", "Con patatas", 9.5));
+		BD.cerrarBD(con);
 	}
 }
