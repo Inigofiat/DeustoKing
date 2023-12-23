c352bf8789b1bf3509c23186e791e4f11b11398e
Naroa Azcona <naroa.azcona@opendeusto.es>
2023-12-23 16:49:17 +0100

    Clase Cupon creada

---
+++ DeustoKing/src/deustoking/Cupon.java
@@ -0,0 +1,52 @@
+package deustoking;
+
+public class Cupon {
+	private int minPuntos;
+	private float descuento;
+	private String foto;
+	private String descripción;
+	
+	public Cupon() {
+		super();
+	}
+	
+	public Cupon(int minPuntos, float descuento, String foto, String descripción) {
+		super();
+		this.minPuntos = minPuntos;
+		this.descuento = descuento;
+		this.foto = foto;
+		this.descripción = descripción;
+	}
+	
+	public int getMinPuntos() {
+		return minPuntos;
+	}
+	public void setMinPuntos(int minPuntos) {
+		this.minPuntos = minPuntos;
+	}
+	public float getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getDescripción() {
+		return descripción;
+	}
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cupon [minPuntos=" + minPuntos + ", descuento=" + descuento + ", foto=" + foto + ", descripción="
+				+ descripción + "]";
+	}
+	
+}
