d5a996a38e552c695a5a4a2fab97fce29f973417
Iñigo Fiat <inigo.fiat@opendeusto.es>
2023-12-23 13:29:50 +0100

    Modificación para que solo se pinte la columna de comensales

--- DeustoKing/src/ventanas/RendererReserva.java
+++ DeustoKing/src/ventanas/RendererReserva.java
@@ -34,54 +34,58 @@ public class RendererReserva implements TableCellRenderer{
 		
 		String numComensales = table.getValueAt(row, 2).toString();
 		int numComensalesInt = Integer.parseInt(numComensales);
-        if (numComensalesInt == 2) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-            label.setOpaque(true);
-            label.setBackground(new Color(138,249,132));
-            label.setForeground(Color.BLACK);
-        } else if (numComensalesInt == 3) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-            label.setOpaque(true);
-            label.setBackground(new Color(250,252,127));
-            label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==4) {
-        	 label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(250,72,252));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==5) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(255,91,91));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==6) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(141, 110, 99));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==7) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(179,249,247));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==8) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(249,157,76));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==9) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(195,192,189));
-             label.setForeground(Color.BLACK);
-        } else if(numComensalesInt==10) {
-        	label.setHorizontalAlignment(JLabel.CENTER);
-        	 label.setOpaque(true);
-             label.setBackground(new Color(81,92,252));
-             label.setForeground(Color.BLACK);
-        }
-		
+		if(column==2) {
+			 if (numComensalesInt == 2) {
+		        	label.setHorizontalAlignment(JLabel.CENTER);
+		            label.setOpaque(true);
+		            label.setBackground(new Color(138,249,132));
+		            label.setForeground(Color.BLACK);
+		        } else if (numComensalesInt == 3) {
+		        	label.setHorizontalAlignment(JLabel.CENTER);
+		            label.setOpaque(true);
+		            label.setBackground(new Color(250,252,127));
+		            label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==4) {
+		        	 label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(250,72,252));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==5) {
+		        	label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(255,91,91));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==6) {
+		        	label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(141, 110, 99));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==7) {
+		        	 label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(179,249,247));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==8) {
+		        	 label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(249,157,76));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==9) {
+		        	 label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(195,192,189));
+		             label.setForeground(Color.BLACK);
+		        } else if(numComensalesInt==10) {
+		        	 label.setHorizontalAlignment(JLabel.CENTER);
+		        	 label.setOpaque(true);
+		             label.setBackground(new Color(81,92,252));
+		             label.setForeground(Color.BLACK);
+		        }
+				
+		}
+       
 		return label;
 	}
+
 
 }
