e02c79431c82b17dbb878f9eaa8a8485e6217fe6
Iñigo Fiat <inigo.fiat@opendeusto.es>
2023-12-27 14:21:46 +0100

    Creación del cupón test

---
+++ DeustoKing/Test/Test/CuponTest.java
@@ -0,0 +1,74 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CuponTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCupon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCuponIntFloatStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinPuntos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMinPuntos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescuento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescuento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFoto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFoto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescripción() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescripción() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
