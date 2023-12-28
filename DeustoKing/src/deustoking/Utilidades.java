1c7c2757e38fd4f4e0c43e40d0981dcd81800507
Naroa Azcona <naroa.azcona@opendeusto.es>
2023-12-28 20:48:44 +0100

    JavaDoc

--- DeustoKing/src/deustoking/Utilidades.java
+++ DeustoKing/src/deustoking/Utilidades.java
@@ -8,9 +8,22 @@ public class Utilidades {
 	
 public static SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 	
+	/***
+	 * Este método convierte de un Date a String
+	 * 
+	 * @param d Date, la fehca
+	 * @return devuelve la fecha en formato String
+	 */
 	public static String dateToString(Date d) {
 		return sdf.format(d);
 	}
+	
+	/***
+	 * Este método convierte un String a Date
+	 * 
+	 * @param s String
+	 * @return devuelve el String en formato Date
+	 */
 	
 	public static Date stringToDate(String s) {
 		try {
