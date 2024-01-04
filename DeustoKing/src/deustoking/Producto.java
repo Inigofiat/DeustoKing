6a53472d4deafeb1a219f4243637c3dab5d2d5f6
Iñigo Fiat <inigo.fiat@opendeusto.es>
2024-01-04 23:32:14 +0100

    Creación del enum TipoProducto y modificaciones en la clase Producto

--- DeustoKing/src/deustoking/Producto.java
+++ DeustoKing/src/deustoking/Producto.java
@@ -1,14 +1,20 @@
 package deustoking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producto {
 	private int idP;
 	private String nombre;
     private String descripcion;
-    private double precio;
+    private float precio;
     private int cantidad;
     private String modificacion;
+    private TipoProducto tipoProducto;
+    private String imagen;
+    private List<Producto> listaProductos;
     
-	public Producto(int idP, String nombre, String descripcion, double precio, int cantidad, String modificacion) {
+	public Producto(int idP, String nombre, String descripcion, float precio, int cantidad, String modificacion, TipoProducto producto, String imagen,List<Producto> productos) {
 		super();
 		this.idP = idP;
 		this.nombre = nombre;
@@ -16,6 +22,18 @@ public class Producto {
 		this.precio = precio;
 		this.cantidad=cantidad;
 		this.modificacion=modificacion;
+	}
+	
+	public Producto(int idP2, String nombre2, String ingredientes, float precio2, int cantidad2, String modificacion2,
+			 String imagen2, ArrayList<Producto> productos2) {
+		this.idP=idP2;
+		this.nombre=nombre2;
+		this.descripcion=ingredientes;
+		this.precio=precio2;
+		this.cantidad=cantidad2;
+		this.modificacion=modificacion2;
+		this.imagen=imagen2;
+		this.listaProductos=productos2;
 	}
 	public int getIdP() {
 		return idP;
@@ -35,10 +53,10 @@ public class Producto {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	public double getPrecio() {
+	public float getPrecio() {
 		return precio;
 	}
-	public void setPrecio(double precio) {
+	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
 	
@@ -54,12 +72,52 @@ public class Producto {
 	public void setModificacion(String modificacion) {
 		this.modificacion = modificacion;
 	}
-	@Override
-	public String toString() {
-		return "Producto [idP=" + idP + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
-				+ ", cantidad=" + cantidad + ", modificacion=" + modificacion + "]";
+	public TipoProducto getProducto() {
+		return tipoProducto;
+	}
+	public void setProducto(TipoProducto producto) {
+		this.tipoProducto = producto;
 	}
 	
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	
+	public List<Producto> getProductos() {
+		return listaProductos;
+	}
+	public void setProductos(List<Producto> productos) {
+		this.listaProductos = productos;
+	}
+//	@Override
+//	public String toString() {
+//		return "Producto [idP=" + idP + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
+//				+ ", cantidad=" + cantidad + ", modificacion=" + modificacion + "]";
+//	}
+	
+	@Override
+	public String toString() {
+	//	String texto =  nombre + ": " + descripcion + "(" + cantidad +")" + modificacion;
+		
+		String texto = String.format("NOMBRE: %90s\nDESCRIPCIÓN: %90s\nCANTIDAD: %90d\nMODIFICACIÓN: %90s\nLISTA: %90s\n", nombre,descripcion,cantidad,modificacion, listaProductos);
+		
+		
+		
+		/*String textoConFormato = "";
+		for(int i=0;i<texto.length();i++) {
+			textoConFormato = textoConFormato + texto.charAt(i);
+			if(i%30==0) {
+				textoConFormato = textoConFormato + "\n";
+			}
+		}
+		
+		
+		return textoConFormato;*/
+		return texto;
+	}
 	
 
 
---
+++ DeustoKing/src/deustoking/TipoProducto.java
@@ -0,0 +1,5 @@
+package deustoking;
+
+public enum TipoProducto {
+	ENTRANTE, ENSALADA, HAMBURGUESA, POSTRE, BEBIDA, INFANTIL, OTRO, SMASH, MENU
+}
